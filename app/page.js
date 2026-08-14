828c1c6fc3db1bdb9700657818f4446a85e34a7d
Priyanshuiscoding <priyanshuahirwar1002@gmail.com>
2026-08-14 15:42:00 +0530

    Replace fatty liver video with detox camp

--- app/page.js
+++ app/page.js
@@ -93,9 +93,9 @@ const treatmentDepartments = [
 
 const videoTestimonials = [
   {
-    title: "Can Fatty Liver Be Reversed Naturally?",
-    titleHi: "\u0915\u094d\u092f\u093e \u092b\u0948\u091f\u0940 \u0932\u093f\u0935\u0930 \u092a\u094d\u0930\u093e\u0915\u0943\u0924\u093f\u0915 \u0930\u0942\u092a \u0938\u0947 \u0920\u0940\u0915 \u0939\u094b \u0938\u0915\u0924\u093e \u0939\u0948?",
-    youtubeUrl: "https://youtu.be/w3v54zlSkac?si=basYZV1i1NfW7MGS"
+    title: "One Day Detox Camp | Real Participants' Experience",
+    titleHi: "\u090f\u0915 \u0926\u093f\u0935\u0938\u0940\u092f \u0921\u093f\u091f\u0949\u0915\u094d\u0938 \u0915\u0948\u0902\u092a | \u092a\u094d\u0930\u0924\u093f\u092d\u093e\u0917\u093f\u092f\u094b\u0902 \u0915\u093e \u0935\u093e\u0938\u094d\u0924\u0935\u093f\u0915 \u0905\u0928\u0941\u092d\u0935",
+    youtubeUrl: "https://youtu.be/ciIr3NWpGac?si=RZzojIr2Nhu200Qp"
   },
   {
     title: "Herpes Zoster Treatment in 3 Days | Ayurvedic Care",
